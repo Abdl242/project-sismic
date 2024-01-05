e6836fd1fb8fb9c3834b06a241a46a0da1e9cefa
Abdl242 <91426120+Abdl242@users.noreply.github.com>
2024-01-05 12:18:27 +0100

    update

--- main copy.py
+++ main copy.py
@@ -8,46 +8,18 @@ import logging
 import json
 import sys
 
-from google.cloud import pubsub_v1
-
-
 echo_uri = 'wss://www.seismicportal.eu/standing_order/websocket'
 PING_INTERVAL = 15
-
-
-def publish(message):
-    project_id = 'your-project-id'
-    topic_name = 'your-topic'
-
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_name)
-
-    # Message to be sent
-    message_data = message
-
-    # Convert the message to bytes
-    message_data = json.dumps(message_data)
-    message_data_bytes = message_data.encode('utf-8')
-
-    # Create a Pub/Sub message
-    message = pubsub_v1.types.PubsubMessage(data=message_data_bytes)
-
-    # Publish the message
-    response = publisher.publish(topic_path, messages=[message])
-
-
 
 #You can modify this function to run custom process on the message
 def myprocessing(message):
     try:
-        data = json.loads(message)["data"]
-        msg = {"time":data["properties"]["time"],
-       "lat":data["properties"]["lat"],
-       "lon":data["properties"]["lon"],
-       "magnitude":data["properties"]["mag"],
-       "type":data["properties"]["action"]}
-        
-        publish(msg)
+        data = json.loads(message)
+        info = data['data']['properties']
+        info['action'] = data['action']
+        print(data)
+        print(info)
+        logging.info('>>>> {action:7} event from {auth:7}, unid:{unid}, T0:{time}, Mag:{mag}, Region: {flynn_region}'.format(**info))
     except Exception:
         logging.exception("Unable to parse json message")
 
